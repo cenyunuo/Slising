83d2a0f6e48ad4b406ae4b677e4dfc255b8a9191
cenyunuo <62534969+cenyunuo@users.noreply.github.com>
2022-06-03 16:37:13 +0800

    Update tsp.py

--- concorde/tsp.py
+++ concorde/tsp.py
@@ -1,16 +1,11 @@
-# -*- coding: utf-8 -*-
 from __future__ import division, print_function
-
 from collections import namedtuple
-import uuid
-
 from concorde._concorde import _CCutil_gettsplib, _CCtsp_solve_dat
 from concorde.util import write_tsp_file, EDGE_WEIGHT_TYPES
+import numpy as np
+import uuid
 
-ComputedTour = namedtuple('ComputedTour', [
-    'tour', 'optimal_value', 'success', 'found_tour', 'hit_timebound'
-])
-
+ComputedTour = namedtuple('ComputedTour', ['tour', 'optimal_value', 'success', 'found_tour', 'hit_timebound'])
 
 class TSPSolver(object):
 
@@ -52,16 +47,54 @@ class TSPSolver(object):
             self.nearest()
         return self.part_sol
 
-    def __str__(self):
-        if self._data is None:
-            return "Uninitialized TSPSolver"
-        else:
-            return "TSPSolver with {} nodes".format(self._ncount)
+    def CalD(self, cities):
+        x = self._data.x[cities].reshape(len(cities), 1)
+        y = self._data.y[cities].reshape(len(cities), 1)
+        d = np.sqrt((x - x.T)**2 + (y - y.T)**2)
+        return d/d.max()
+
+    def CalJ(self, cities, d):
+        J = np.zeros((len(cities)**2, len(cities)**2))
+        for i in range(len(cities)):
+            for j in range(len(cities)):
+                I = i*len(cities) + j
+                for ip in range(len(cities)):
+                    for jp in range(len(cities)):
+                        Ip = ip*len(cities)+jp
+                        J[I, Ip] = (i==ip) + (j==jp)
+                        if (jp==j-1) or (jp==j+1):
+                            J[I, Ip] = J[I,Ip] + 0.5*d[i, ip]/4
+        return J
+
+    def arr2seq(self, arr):
+        return np.argmax(arr, axis=1)
+
+    def CalEne(self, cities, spin, d):
+        S = spin.reshape(len(cities), len(cities))
+        Ha_0 = (S.sum(axis=0) + len(cities) - 2)**2
+        Ha_1 = (S.sum(axis=1) + len(cities) - 2)**2
+        HA = Ha_0.sum() + Ha_1.sum()
+        HB = 0
+        for j in range(len(cities)-1):
+            for i in range(len(cities)):
+                for ip in range(len(cities)):
+                    HB += d[i,ip]*(S[i, j]+1)*(S[ip, j+1]+1)
+        return HA, HB
+
+    def anneal(self, cities):
+        n_step = 10001
+        spins = np.zeros((len(cities)**2, n_step))
+        spins[:,0] = 2*np.eye(len(cities)).reshape((len(cities)**2, 1))-1
+        d = self.CalD(cities)
+        J = self.CalJ(cities, d) - 5*np.eye(len(cities)**2)
+
+
+    def sliding(self, pos, size_w):
+        cities = self.part_sol[pos: pos+size_w+1]
+        self.part_sol[pos: pos+size_w+1] = self.anneal(cities)
+
 
     def concorde(self, time_bound=-1, verbose=False, random_seed=0):
         name = str(uuid.uuid4().hex)[0:9]
-        res = _CCtsp_solve_dat(
-            self._ncount, self._data, name,
-            time_bound, verbose, random_seed
-        )
+        res = _CCtsp_solve_dat(self._ncount, self._data, name, time_bound, verbose, random_seed)
         return ComputedTour(*res)
